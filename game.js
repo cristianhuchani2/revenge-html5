51224997764c935733f1fa20db4924c72d1d062f
cristianhuchani2 <cristianhuchani25@gmail.com>
2026-01-02 11:58:55 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,152 +1,176 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-var bg = new Image();
-bg.src = "background.png";
+var bg = new Image(); bg.src = "background.png";
+var dinoImg = new Image(); dinoImg.src = "dino.png";
+var birdImg = new Image(); birdImg.src = "bird.png";
+var cactusImg = new Image(); cactusImg.src = "cactus.png";
 
-var dinoImg = new Image();
-dinoImg.src = "dino.png";
+var GRAVITY = 0.4;
+var GROUND_Y = 230;
 
-var birdImg = new Image();
-birdImg.src = "bird.png";
-
-var cactusImg = new Image();
-cactusImg.src = "cactus.png";
-
-var GRAVITY = 0.35;
-var GROUND_Y = 230; // bajado un poco
+var keys = { left:false, right:false };
 
 var dino = {
-    x: canvas.width / 2 - 37,
+    x: canvas.width/2 - 37,
     y: GROUND_Y,
     w: 74,
     h: 72,
+    vx: 0,
     vy: 0,
+    speed: 2.2,
     dir: 1,
-    jump: function () {
-        if (this.y >= GROUND_Y) this.vy = -7;
-    },
-    update: function () {
-        this.vy += GRAVITY;
-        this.y += this.vy;
-        if (this.y > GROUND_Y) {
-            this.y = GROUND_Y;
-            this.vy = 0;
-        }
-    },
-    draw: function () {
-        ctx.save();
-        ctx.translate(this.x + this.w / 2, this.y + this.h / 2);
-        ctx.rotate(this.dir === 1 ? Math.PI / 2 : -Math.PI / 2);
-        ctx.drawImage(dinoImg, -this.h / 2, -this.w / 2, this.h, this.w);
-        ctx.restore();
-    }
+    onGround: true
 };
-
-var bullets = [];
-function shoot() {
-    bullets.push({
-        x: dino.x + dino.w / 2,
-        y: dino.y + dino.h / 2,
-        vx: dino.dir * 6
-    });
-}
 
 var enemies = [];
 
 function spawnEnemy() {
-    var fromLeft = Math.random() < 0.5;
+    var side = Math.random() < 0.5 ? "left" : "right";
     enemies.push({
-        x: fromLeft ? -60 : canvas.width + 60,
+        x: side === "left" ? -60 : canvas.width + 60,
         y: GROUND_Y + 10,
         w: 52,
         h: 100,
-        vx: fromLeft ? 2 : -2,
-        img: Math.random() < 0.5 ? cactusImg : birdImg
+        vx: side === "left" ? 1.4 : -1.4,
+        dir: side === "left" ? 1 : -1,
+        type: "cactus"
     });
 }
 
-var timer = 0;
-
-function drawButtons() {
-    var left = document.getElementById("left");
-    var right = document.getElementById("right");
-    var jump = document.getElementById("jump");
-
-    left.innerHTML = right.innerHTML = jump.innerHTML = "";
-
-    drawArrow(left, "left");
-    drawArrow(right, "right");
-    drawArrow(jump, "up");
+function spawnBird() {
+    var side = Math.random() < 0.5 ? "left" : "right";
+    enemies.push({
+        x: side === "left" ? -80 : canvas.width + 80,
+        y: 170,
+        w: 75,
+        h: 75,
+        vx: side === "left" ? 1.8 : -1.8,
+        dir: side === "left" ? 1 : -1,
+        type: "bird"
+    });
 }
 
-function drawArrow(el, dir) {
-    var c = document.createElement("canvas");
-    c.width = c.height = 60;
-    var x = c.getContext("2d");
+var spawnTimer = 0;
 
-    x.fillStyle = "#333";
-    x.fillRect(0,0,60,60);
+function update() {
 
-    x.fillStyle = "#aaa";
-    x.beginPath();
-    if (dir === "left") {
-        x.moveTo(40,10); x.lineTo(20,30); x.lineTo(40,50);
-    }
-    if (dir === "right") {
-        x.moveTo(20,10); x.lineTo(40,30); x.lineTo(20,50);
-    }
-    if (dir === "up") {
-        x.moveTo(10,40); x.lineTo(30,20); x.lineTo(50,40);
-    }
-    x.fill();
+    // MOVIMIENTO SOLO SI SE PRESIONA
+    dino.vx = 0;
+    if (keys.left) { dino.vx = -dino.speed; dino.dir = -1; }
+    if (keys.right) { dino.vx = dino.speed; dino.dir = 1; }
 
-    el.appendChild(c);
+    dino.x += dino.vx;
+
+    // NO SALIR DE PANTALLA
+    if (dino.x < 0) dino.x = 0;
+    if (dino.x + dino.w > canvas.width) dino.x = canvas.width - dino.w;
+
+    // GRAVEDAD
+    dino.vy += GRAVITY;
+    dino.y += dino.vy;
+
+    if (dino.y >= GROUND_Y) {
+        dino.y = GROUND_Y;
+        dino.vy = 0;
+        dino.onGround = true;
+    }
+
+    spawnTimer++;
+    if (spawnTimer % 120 === 0) spawnEnemy();
+    if (spawnTimer % 200 === 0) spawnBird();
+
+    for (var i = enemies.length-1; i >= 0; i--) {
+        var e = enemies[i];
+        e.x += e.vx;
+
+        // COLISION
+        if (
+            dino.x < e.x + e.w &&
+            dino.x + dino.w > e.x &&
+            dino.y < e.y + e.h &&
+            dino.y + dino.h > e.y
+        ) {
+            alert("Game Over");
+            location.reload();
+        }
+
+        if (e.x < -200 || e.x > canvas.width + 200) {
+            enemies.splice(i,1);
+        }
+    }
 }
 
-drawButtons();
+function drawRotated(img, x, y, w, h, dir) {
+    ctx.save();
+    ctx.translate(x + w/2, y + h/2);
+    ctx.rotate(dir === 1 ? Math.PI/2 : -Math.PI/2);
+    ctx.drawImage(img, -h/2, -w/2, h, w);
+    ctx.restore();
+}
 
-document.getElementById("left").ontouchstart =
-document.getElementById("left").onmousedown = function () {
-    dino.x -= 5;
-    dino.dir = -1;
-};
+function drawButton(x, y, type) {
+    ctx.fillStyle = "#333";
+    ctx.fillRect(x, y, 64, 64);
+    ctx.strokeStyle = "#777";
+    ctx.strokeRect(x, y, 64, 64);
 
-document.getElementById("right").ontouchstart =
-document.getElementById("right").onmousedown = function () {
-    dino.x += 5;
-    dino.dir = 1;
-};
+    ctx.fillStyle = "#ccc";
+    ctx.beginPath();
 
-document.getElementById("jump").ontouchstart =
-document.getElementById("jump").onmousedown = function () {
-    dino.jump();
-    shoot();
-};
+    if (type === "left") {
+        ctx.moveTo(x+20, y+32);
+        ctx.lineTo(x+44, y+16);
+        ctx.lineTo(x+44, y+48);
+    } else if (type === "right") {
+        ctx.moveTo(x+44, y+32);
+        ctx.lineTo(x+20, y+16);
+        ctx.lineTo(x+20, y+48);
+    } else {
+        ctx.rect(x+24, y+20, 16, 24);
+    }
 
-function loop() {
+    ctx.fill();
+}
+
+function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     if (bg.complete) ctx.drawImage(bg,0,0);
 
-    dino.update();
-    dino.draw();
+    drawRotated(dinoImg, dino.x, dino.y, dino.w, dino.h, dino.dir);
 
-    for (var i=0;i<bullets.length;i++){
-        bullets[i].x += bullets[i].vx;
-        ctx.fillStyle="#fff";
-        ctx.fillRect(bullets[i].x, bullets[i].y, 6, 2);
+    for (var i=0;i<enemies.length;i++) {
+        var e = enemies[i];
+        if (e.type === "bird")
+            drawRotated(birdImg, e.x, e.y, e.w, e.h, e.dir);
+        else
+            ctx.drawImage(cactusImg, e.x, e.y, e.w, e.h);
     }
 
-    for (var e=0;e<enemies.length;e++){
-        enemies[e].x += enemies[e].vx;
-        ctx.drawImage(enemies[e].img, enemies[e].x, enemies[e].y,
-                      enemies[e].w, enemies[e].h);
-    }
-
-    timer++;
-    if (timer % 120 === 0) spawnEnemy();
-
-    requestAnimationFrame(loop);
+    drawButton(20, canvas.height-74, "left");
+    drawButton(100, canvas.height-74, "right");
+    drawButton(canvas.width-84, canvas.height-74, "jump");
 }
 
+function loop() {
+    update();
+    draw();
+    requestAnimationFrame(loop);
+}
 loop();
+
+// CONTROLES TÁCTILES
+function bind(id, down, up) {
+    var el = document.getElementById(id);
+    el.addEventListener("touchstart", function(e){e.preventDefault();down();});
+    el.addEventListener("touchend", function(e){e.preventDefault();up();});
+}
+
+bind("left", function(){keys.left=true;}, function(){keys.left=false;});
+bind("right", function(){keys.right=true;}, function(){keys.right=false;});
+bind("jump", function(){
+    if (dino.onGround) {
+        dino.vy = -7;
+        dino.onGround = false;
+    }
+}, function(){});
